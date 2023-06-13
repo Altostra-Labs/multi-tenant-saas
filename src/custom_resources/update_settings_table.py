9b28e8b2c4c7b4347d3a20897aa2607c3ac13282
Shlomi Borovitz <shlomib@altostra.com>
2023-06-13 16:02:20 +0300

    Revert "Make lambda run"
    This reverts commit 1aa73361dd6f70e945f90024ce6e98365a76df1b.

--- src/custom_resources/update_settings_table.py
+++ src/custom_resources/update_settings_table.py
@@ -1,56 +1,53 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 import os
-import sys
 print(os.getcwd())
-print(sys.path)
 
 import json
 import boto3
-# import logger
+import logger
 
 from crhelper import CfnResource
 helper = CfnResource()
 
-# try:
-#     dynamodb = boto3.resource('dynamodb')
-# except Exception as e:
-#     helper.init_failure(e)
+try:
+    dynamodb = boto3.resource('dynamodb')
+except Exception as e:
+    helper.init_failure(e)
     
     
 @helper.create
 @helper.update
 def do_action(event, _):
-    pass
-    # """ Called as part of bootstrap template. 
-    #     Inserts/Updates Settings table based upon the resources deployed inside bootstrap template
-    #     We use these settings inside tenant template
+    """ Called as part of bootstrap template. 
+        Inserts/Updates Settings table based upon the resources deployed inside bootstrap template
+        We use these settings inside tenant template
 
-    # Args:
-    #         event ([type]): [description]
-    #         _ ([type]): [description]
-    # """
-    # logger.info("Updating settings")
+    Args:
+            event ([type]): [description]
+            _ ([type]): [description]
+    """
+    logger.info("Updating settings")
 
-    # settings_table_name = event['ResourceProperties']['SettingsTableName']
-    # cognitoUserPoolId = os.environ['cognitoUserPoolId']
-    # cognitoUserPoolClientId = os.environ['cognitoUserPoolClientId']
+    settings_table_name = event['ResourceProperties']['SettingsTableName']
+    cognitoUserPoolId = os.environ['cognitoUserPoolId']
+    cognitoUserPoolClientId = os.environ['cognitoUserPoolClientId']
 
-    # table_system_settings = dynamodb.Table(settings_table_name)
+    table_system_settings = dynamodb.Table(settings_table_name)
 
-    # response = table_system_settings.put_item(
-    #         Item={
-    #                 'settingName': 'userPoolId-pooled',
-    #                 'settingValue' : cognitoUserPoolId
-    #             }
-    #         )
+    response = table_system_settings.put_item(
+            Item={
+                    'settingName': 'userPoolId-pooled',
+                    'settingValue' : cognitoUserPoolId
+                }
+            )
 
-    # response = table_system_settings.put_item(
-    #         Item={
-    #                 'settingName': 'appClientId-pooled',
-    #                 'settingValue' : cognitoUserPoolClientId
-    #             }
-    #         )
+    response = table_system_settings.put_item(
+            Item={
+                    'settingName': 'appClientId-pooled',
+                    'settingValue' : cognitoUserPoolClientId
+                }
+            )
 
 @helper.delete
 def do_nothing(_, __):
